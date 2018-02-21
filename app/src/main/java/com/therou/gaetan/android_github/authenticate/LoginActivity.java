e37d2c1da90bf8a848293b1b4d30e1624fce41ce
Gaetan Therou <gaetan.therou@epitech.eu>
2018-02-21 23:31:20 +0100

    Basic login view

---
+++ app/src/main/java/com/therou/gaetan/android_github/authenticate/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.therou.gaetan.android_github.authenticate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.therou.gaetan.android_github.R;
+
+/**
+ * Created by gaetan
+ *
+ * On 18-02-21 at 22:24.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle pSavedInstanceState) {
+        super.onCreate(pSavedInstanceState);
+        setContentView(R.layout.activity_authenticate_login);
+    }
+}
